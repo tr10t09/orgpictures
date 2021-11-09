212a83576f94567f0ce7f908f1c63a78e94650b7
Thorsten Rendel <tho.ren@gmx.de>
2021-11-09 20:13:06 +0100

    class update

--- fileprops.py
+++ fileprops.py
@@ -1,5 +1,6 @@
 import os
 import hashlib
+from PIL import Image
 from datetime import datetime, timedelta
 
 class FileProps():
@@ -36,3 +37,30 @@ class FileProps():
     def __str__(self):
         return '{};{};{};{}'.format(self.fname, self.hashval, self.fsize, self.fmtime)
     
+class FilePropsImg(FileProps):
+    def __init__(self):
+        self.exittime = self.get_exiftime()
+    @classmethod
+    def preprocess_exif(self, data):
+        data = data.strip()
+        data = data.strip('\x00')
+        return data
+
+    def get_exiftime(self):
+        with Image.open(os.path.join(self.fname)) as img:
+            exif = img._getexif() 
+            
+        ts = self.preprocess_exif(exif[306])
+            
+        date = ts.split(' ')[0]
+        time = ts.split(' ')[1]
+        year = datetime.strptime(date, '%Y:%m:%d').strftime('%Y')
+        yearm = datetime.strptime(date, '%Y:%m:%d').strftime('%Y_%m')
+        return ts
+
+    def __str__(self):
+        return '{};{};{};{};{}'.format(self.fname, self.hashval, self.fsize, self.fmtime, self.exittime)
+    
+
+pic = FilePropsImg("/home/trendel/Bilder/_backup_hd/processed_img/P5121958_1.JPG")
+print(pic)
